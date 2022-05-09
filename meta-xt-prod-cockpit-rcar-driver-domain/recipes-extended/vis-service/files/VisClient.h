2375700671119f72443ce4dd4c708d1ef48d3d8d
Ihor Usyk <ihor_usyk@epam.com>
2022-05-10 00:30:52 +0200

    domd: introduce vis-service
    vis-service provides the communication (speed value)
    from demo application and IC.
    qtbase - ssl includes in the project because aos-vis uses
    ssl connection.
    
    Signed-off-by: Ihor Usyk <ihor_usyk@epam.com>
    Reviewed-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

---
+++ meta-xt-prod-cockpit-rcar-driver-domain/recipes-extended/vis-service/files/VisClient.h
@@ -0,0 +1,57 @@
+#ifndef VIS_CLIENT_H
+#define VIS_CLIENT_H
+
+#undef QT_NO_SSL
+#include <linux/rpmsg.h>
+#include <fcntl.h>
+#include <QtCore/QObject>
+#include <QtWebSockets/QWebSocket>
+#include <QtNetwork/QSslError>
+#include <QUuid>
+
+enum SubscrState {
+	StateInit = 0,
+	StateGetValues = 1,
+	StateSubscribe = 2,
+	StateReady = 3
+};
+
+class VisClient : public QObject
+{
+    Q_OBJECT
+public:
+    VisClient(QObject *parent, const QString &url, const QString& rpmsg);
+    virtual ~VisClient();
+
+    Q_INVOKABLE void connectTo();
+    Q_INVOKABLE void disconnect();
+    Q_INVOKABLE void sendMessage(const QString &message);
+    Q_INVOKABLE bool IsConnected()const;
+
+Q_SIGNALS:
+    void connected();
+    void disconnected();
+    void error(const QString &message);
+    void messageReceived(const QString &message);
+
+private Q_SLOTS:
+    void onConnected();
+    void onDisconnected();
+    void onError(QAbstractSocket::SocketError error);
+    void onSslErrors(const QList<QSslError> &errors);
+    void onTextMessageReceived(const QString &message);
+
+    QString getSubscriptionId(const QString &message)const;
+    int getSpeed(const QString &message)const;
+private:
+    QString mUrl;
+    QWebSocket mWebSocket;
+    int mFd;
+    int mFdept;
+    QUuid mID;
+    struct rpmsg_endpoint_info mEptInfo;
+    SubscrState mState;
+    QString mSubscriptionId;
+};    
+
+#endif // VIS_CLIENT_H
