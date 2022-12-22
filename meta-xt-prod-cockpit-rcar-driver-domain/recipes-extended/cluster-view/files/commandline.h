7002fcc7aaf558da538a22a749a9fd28b1c9f283
Ihor Usyk <ihor_usyk@epam.com>
2022-12-22 15:50:39 +0100

    cluster-view: add new background and 'epam' image
    At the startup, instead of black square for android
    the image with 'Epam' logo is displayed until android starts
    
    Signed-off-by: Ihor Usyk <ihor_usyk@epam.com>
    Reviewed-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

--- meta-xt-prod-cockpit-rcar-driver-domain/recipes-extended/cluster-view/files/commandline.h
+++ meta-xt-prod-cockpit-rcar-driver-domain/recipes-extended/cluster-view/files/commandline.h
@@ -9,11 +9,14 @@ class CommandLine: public QObject
     Q_OBJECT
     Q_PROPERTY(QString urlValue READ urlValue WRITE setUrlValue NOTIFY urlValueChanged)
     Q_PROPERTY(int modeValue READ modeValue WRITE setModeValue NOTIFY modeValueChanged)
+    Q_PROPERTY(int firstStart READ firstStart WRITE setFirstStart NOTIFY firstStartChanged)
 public:
     CommandLine();
+    ~CommandLine();
 
     Q_INVOKABLE QString getUrl() const;
     Q_INVOKABLE int     getMode() const;
+    Q_INVOKABLE int     getFirstStart() const;
 
     QString urlValue();
     void setUrlValue(QString url);
@@ -21,13 +24,18 @@ public:
     int modeValue();
     void setModeValue(int mode);
 
+    int firstStart();
+    void setFirstStart(int started);
+
 signals:
     void urlValueChanged();
     void modeValueChanged();
+    void firstStartChanged();
 
 private:
     QString mUrl;
     int mMode;
+    int mFirstStart;
 };
 
 #endif // COMMANDLINE_H
