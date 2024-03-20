493518921d262c1e6f77af53e0e9abe392e9f1ac
Ihor Usyk <ihor_usyk@epam.com>
2024-03-20 19:22:38 +0100

    vis-service: redesign, use qt slot-signals to process the message
    Redesign has been done to separate the functionality accumulated in
    VisClient. Now, it is proposed to use the consumers that get the messges and
    send the corresponded commands to teh instrument cluster.
    Each consumer corresponds to the cluster command
    For example: SpeedConsumer, checks the message if there is an update for speed,
    if yes - sends the update to the IC.
    
    Signed-off-by: Ihor Usyk <ihor.usyk@gmail.com>

---
+++ meta-xt-prod-cockpit-rcar-driver-domain/recipes-extended/vis-service/files/Consumers.cpp
@@ -0,0 +1,351 @@
+
+#include <linux/rpmsg.h>
+#include <QSharedPointer>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QVector>
+#include <QSharedPointer>
+
+#ifndef _TEST_
+#include <linux/r_taurus_cluster_protocol.h>
+#else
+#define CLUSTER_SPEED                   0x1
+#define CLUSTER_GEAR                    0x2
+#define CLUSTER_RPM                     0x3
+#define CLUSTER_TURN                    0x4
+#define CLUSTER_DOOR_OPEN               0x5
+#define CLUSTER_FOG_LIGHTS_BACK         0x6
+#define CLUSTER_FOG_LIGHTS_FRONT        0x7
+#define CLUSTER_HIGH_BEAMS_LIGHT        0x8
+#define CLUSTER_HIGH_ENGINE_TEMPERATURE 0x9
+#define CLUSTER_LOW_BATTERY             0xA
+#define CLUSTER_LOW_BEAMS_LIGHTS        0xB
+#define CLUSTER_LOW_FUEL                0xC
+#define CLUSTER_LOW_OIL                 0xD
+#define CLUSTER_LOW_TIRE_PRESSURE       0xE
+#define CLUSTER_SEAT_BELT               0xF
+#define CLUSTER_SIDE_LIGHTS             0x10
+#define CLUSTER_BATTERY_ISSUE           0x11
+#define CLUSTER_AUTO_LIGHTING_ON        0x12
+#define CLUSTER_ACTIVE                  0xFF
+
+struct taurus_cluster_data {
+uint64_t   value;
+uint16_t   ioctl_cmd;
+};
+
+#endif
+#include <Consumers.h>
+
+class TargetDevice
+{
+private:
+    int device;
+public:
+    TargetDevice()
+    {
+        device = open("/dev/cluster-taurus", O_WRONLY);
+        if(device < 0){
+            throw std::invalid_argument("No device /dev/cluster-taurus");
+        }
+    }
+    ~TargetDevice()
+    {
+        close(device);
+    }
+    void send(const taurus_cluster_data& data)
+    {
+        ioctl(device, data.ioctl_cmd, data.value);
+    }
+};
+
+void BaseConsumer::onMessageReceived(const QString &message)
+{
+    onMessage(message);
+}
+
+void BaseConsumer::send(quint16 cmd, quint64 value)
+{
+    static TargetDevice tDevice;
+    tDevice.send({value, cmd});
+}
+
+QVariant BaseConsumer::getQValue(const QString &msg, const QString& prop)
+{
+    QString res;
+    QByteArray br = msg.toUtf8();
+    QJsonDocument doc = QJsonDocument::fromJson(br);
+    QJsonObject obj = doc.object();
+    QJsonArray arr = obj.value("value").toArray();
+    foreach(const QJsonValue &v, arr){
+        if(v.toObject().contains(prop)) {
+           return v.toObject().value(prop);
+       }
+    }
+    return QVariant{};
+}
+
+void SpeedConsumer::onMessage(const QString &message)
+{
+    static const QString propName {"Signal.Vehicle.Speed"};
+
+    auto value = getQValue(message, propName);
+    if(value.isValid())
+    {
+        send(CLUSTER_SPEED, value.toInt());
+    }
+}
+
+void SpeedConsumer::off()
+{
+    send(CLUSTER_SPEED, 0);
+}
+
+void SpeedConsumer::on()
+{
+    send(CLUSTER_SPEED, 10);
+}
+
+void GearConsumer::onMessage(const QString &message)
+{
+    static const QString propName {"Signal.Drivetrain.Transmission.Gear"};
+
+    auto value = getQValue(message, propName);
+    if(value.isValid())
+    {
+        send(CLUSTER_GEAR, value.toInt());
+    }
+}
+
+void GearConsumer::off()
+{
+    send(CLUSTER_GEAR, 2);
+}
+
+void GearConsumer::on()
+{
+    send(CLUSTER_GEAR, 3);
+}
+
+void RpmConsumer::onMessage(const QString &message)
+{
+    static const QString propName {"Signal.Drivetrain.InternalCombustionEngine.Engine.Speed"};
+
+    auto value = getQValue(message, propName);
+    if(value.isValid())
+    {
+        send(CLUSTER_RPM, value.toInt());
+    }
+}
+
+void RpmConsumer::off()
+{
+    send(CLUSTER_RPM, 0);
+}
+
+void RpmConsumer::on()
+{
+    send(CLUSTER_RPM, 1000);
+}
+
+void TurnConsumer::onMessage(const QString &message)
+{
+    static const QString propName {"Signal.Traffic.Turn.Direction"};
+
+    auto value = getQValue(message, propName);
+    if(value.isValid())
+    {
+        send(CLUSTER_TURN, value.toInt());
+    }
+}
+
+void TurnConsumer::off()
+{
+    send(CLUSTER_TURN, 0);
+}
+
+void TurnConsumer::on()
+{
+    send(CLUSTER_TURN, 1); // right
+}
+
+void DoorConsumer::onMessage(const QString &message)
+{
+    static const QVector<QString> props {
+            "Signal.Cabin.Door.Row1.Left.IsOpen",
+            "Signal.Cabin.Door.Row1.Right.IsOpen",
+            "Signal.Cabin.Door.Row2.Left.IsOpen",
+            "Signal.Cabin.Door.Row2.Right.IsOpen"
+    };
+    for(auto iter = props.begin();iter != props.end(); ++iter)
+    {
+        auto value = getQValue(message, *iter);
+        if(value.isValid())
+        {
+            send(CLUSTER_DOOR_OPEN, value.toInt());
+            break;
+        }
+    }
+}
+
+void DoorConsumer::off()
+{
+    send(CLUSTER_DOOR_OPEN, 0);
+}
+
+void DoorConsumer::on()
+{
+    send(CLUSTER_DOOR_OPEN, 1);
+}
+
+void TrunkConsumer::onMessage(const QString &message)
+{
+    static const QString propName {"Signal.Body.Trunk.IsOpen"};
+
+    auto value = getQValue(message, propName);
+    if(value.isValid())
+    {
+        send(CLUSTER_DOOR_OPEN, value.toInt());
+    }
+}
+
+void TrunkConsumer::off()
+{
+    send(CLUSTER_DOOR_OPEN, 0);
+}
+
+void TrunkConsumer::on()
+{
+    send(CLUSTER_DOOR_OPEN, 1);
+}
+
+void BeltConsumer::onMessage(const QString &message)
+{
+    static const QVector<QString> props {
+            "Signal.Cabin.Seat.Row1.Pos1.IsBelted",
+            "Signal.Cabin.Seat.Row1.Pos2.IsBelted",
+            "Signal.Cabin.Seat.Row2.Pos1.IsBelted",
+            "Signal.Cabin.Seat.Row2.Pos2.IsBelted"
+    };
+    for(auto iter = props.begin();iter != props.end(); ++iter)
+    {
+        auto value = getQValue(message, *iter);
+        if(value.isValid())
+        {
+            send(CLUSTER_SEAT_BELT, value.toInt());
+            break;
+        }
+    }
+}
+
+void BeltConsumer::off()
+{
+    send(CLUSTER_SEAT_BELT, 0);
+}
+
+void BeltConsumer::on()
+{
+    send(CLUSTER_SEAT_BELT, 1);
+}
+
+void LightsConsumer::onMessage(const QString &message)
+{
+    static const QString propLowBean {"Signal.Body.Lights.IsLowBeamOn"};
+    static const QString propHighBean {"Signal.Body.Lights.IsHighBeamOn"};
+    static const QString propFrontFog {"Signal.Body.Lights.IsFrontFogOn"};
+    static const QString propRearFog {"Signal.Body.Lights.IsRearFogOn"};
+
+    auto value = getQValue(message, propLowBean);
+    if(value.isValid())
+    {
+        send(CLUSTER_LOW_BEAMS_LIGHTS, value.toInt());
+    }
+    value = getQValue(message, propHighBean);
+    if(value.isValid())
+    {
+        send(CLUSTER_HIGH_BEAMS_LIGHT, value.toInt());
+    }
+    value = getQValue(message, propFrontFog);
+    if(value.isValid())
+    {
+        send(CLUSTER_FOG_LIGHTS_FRONT, value.toInt());
+    }
+    value = getQValue(message, propRearFog);
+    if(value.isValid())
+    {
+        send(CLUSTER_FOG_LIGHTS_BACK, value.toInt());
+    }
+}
+
+void LightsConsumer::off()
+{
+    send(CLUSTER_LOW_BEAMS_LIGHTS, 0);
+    send(CLUSTER_HIGH_BEAMS_LIGHT, 0);
+    send(CLUSTER_FOG_LIGHTS_FRONT, 0);
+    send(CLUSTER_FOG_LIGHTS_BACK, 0);
+}
+
+void LightsConsumer::on()
+{
+    send(CLUSTER_LOW_BEAMS_LIGHTS, 1);
+    send(CLUSTER_HIGH_BEAMS_LIGHT, 1);
+    send(CLUSTER_FOG_LIGHTS_FRONT, 1);
+    send(CLUSTER_FOG_LIGHTS_BACK, 1);
+}
+
+void TireConsumer::onMessage(const QString &message)
+{
+    static const QString propName {"Signal.Chassis.Axle.Row1.Wheel.Left.Tire.Pressure"};
+
+    auto value = getQValue(message, propName);
+    if(value.isValid())
+    {
+        send(CLUSTER_LOW_TIRE_PRESSURE, value.toInt());
+    }
+}
+
+void TireConsumer::off()
+{
+    send(CLUSTER_LOW_TIRE_PRESSURE, 0);
+}
+
+void TireConsumer::on()
+{
+    send(CLUSTER_LOW_TIRE_PRESSURE, 1);
+}
+
+
+// consumers
+static const QVector<QSharedPointer<BaseConsumer>> consumers {
+    QSharedPointer<BaseConsumer>(new SpeedConsumer()),
+    QSharedPointer<BaseConsumer>(new GearConsumer()),
+    QSharedPointer<BaseConsumer>(new RpmConsumer()),
+    QSharedPointer<BaseConsumer>(new TurnConsumer()),
+    QSharedPointer<BaseConsumer>(new DoorConsumer()),
+    QSharedPointer<BaseConsumer>(new TrunkConsumer()),
+    QSharedPointer<BaseConsumer>(new BeltConsumer()),
+    QSharedPointer<BaseConsumer>(new LightsConsumer()),
+    QSharedPointer<BaseConsumer>(new TireConsumer())
+};
+
+
+ConsumerDispatcher::ConsumerDispatcher()
+{
+    for(auto iter = consumers.begin(); iter != consumers.end();++iter)
+    {
+        connect(this, &ConsumerDispatcher::messageReceived, (*iter).data(), &BaseConsumer::onMessageReceived);
+        connect(this, &ConsumerDispatcher::on, (*iter).data(), &BaseConsumer::on);
+        connect(this, &ConsumerDispatcher::off, (*iter).data(), &BaseConsumer::off);
+    }
+}
+
+void ConsumerDispatcher::onMessageReceived(const QString &message)
+{
+    messageReceived(message);
+}
--- meta-xt-prod-cockpit-rcar-driver-domain/recipes-extended/vis-service/files/VisClient.cpp
+++ meta-xt-prod-cockpit-rcar-driver-domain/recipes-extended/vis-service/files/VisClient.cpp
@@ -18,43 +18,11 @@
 #include <QTimer>
 #include "VisClient.h"
 #include <limits>
-#include <vector>
-#include <linux/r_taurus_cluster_protocol.h>
 
 QT_USE_NAMESPACE
 
-const unsigned long visClientTimeout = 1000;
-struct rpmsg_endpoint_info ept_info = {"rpmsg-openamp-demo-channel", 0x2, 0x1};
-const int not_defined_value = std::numeric_limits<int>::max();
-struct AosVisParameter {
-    QString paramName;
-    uint32_t ctlIOId;
-    int value;
-    std::function<int(const QString & propId, const QString & message)> getValue;
-};
 
-const std::vector<AosVisParameter> aosVisParameters {
-    {"Signal.Vehicle.Speed", CLUSTER_SPEED, not_defined_value, VisClient::getValue},
-    {"Signal.Drivetrain.Transmission.Gear", CLUSTER_GEAR, not_defined_value, VisClient::getValue},
-    {"Signal.Drivetrain.InternalCombustionEngine.Engine.Speed", CLUSTER_RPM, not_defined_value, VisClient::getValue},
-    {"Signal.Traffic.Turn.Direction", CLUSTER_TURN, not_defined_value, VisClient::getTurnDirection},
-    {"Signal.Cabin.Door.Row1.Left.IsOpen", CLUSTER_DOOR_OPEN, not_defined_value, VisClient::getValue},
-    {"Signal.Cabin.Door.Row1.Right.IsOpen", CLUSTER_DOOR_OPEN, not_defined_value, VisClient::getValue},
-    {"Signal.Cabin.Door.Row2.Left.IsOpen", CLUSTER_DOOR_OPEN, not_defined_value, VisClient::getValue},
-    {"Signal.Cabin.Door.Row2.Right.IsOpen", CLUSTER_DOOR_OPEN, not_defined_value, VisClient::getValue},
-    {"Signal.Body.Trunk.IsOpen", CLUSTER_DOOR_OPEN, not_defined_value, VisClient::getBool},
-    {"Signal.Cabin.Seat.Row1.Pos1.IsBelted", CLUSTER_SEAT_BELT, not_defined_value, VisClient::getBeltStatus},
-    {"Signal.Cabin.Seat.Row1.Pos2.IsBelted", CLUSTER_SEAT_BELT, not_defined_value, VisClient::getBeltStatus},
-    {"Signal.Cabin.Seat.Row2.Pos1.IsBelted", CLUSTER_SEAT_BELT, not_defined_value, VisClient::getBeltStatus},
-    {"Signal.Cabin.Seat.Row2.Pos2.IsBelted", CLUSTER_SEAT_BELT, not_defined_value, VisClient::getBeltStatus},
-    {"Signal.Body.Lights.IsLowBeamOn", CLUSTER_LOW_BEAMS_LIGHTS, not_defined_value, VisClient::getValue},
-    {"Signal.Body.Lights.IsHighBeamOn", CLUSTER_HIGH_BEAMS_LIGHT, not_defined_value, VisClient::getValue},
-    {"Signal.Body.Lights.IsFrontFogOn", CLUSTER_FOG_LIGHTS_FRONT, not_defined_value, VisClient::getValue},
-    {"Signal.Body.Lights.IsRearFogOn", CLUSTER_FOG_LIGHTS_BACK, not_defined_value, VisClient::getValue},
-    {"Signal.Chassis.Axle.Row1.Wheel.Left.Tire.Pressure", CLUSTER_LOW_TIRE_PRESSURE, not_defined_value, VisClient::getTireStatus},
-};
-
-VisClient::VisClient(QObject *parent, const QString &url, const QString& rpmsg):QObject(parent),
+VisClient::VisClient(QObject *parent, const QString &url):QObject(parent),
 	mUrl(url),
 	mState(SubscrState::StateInit)
 {
@@ -68,25 +36,17 @@ VisClient::VisClient(QObject *parent, const QString &url, const QString& rpmsg):
     connect(&mWebSocket, &QWebSocket::connected, this, &VisClient::onConnected);
     connect(&mWebSocket, &QWebSocket::disconnected, this, &VisClient::onDisconnected);
     connect(&mWebSocket, &QWebSocket::textMessageReceived, this, &VisClient::onTextMessageReceived);
-
-    mFdept = open("/dev/cluster-taurus", O_WRONLY);
-    if(mFdept < 0){
-        throw std::invalid_argument("No device /dev/cluster-taurus");
-    }
+    // initialize consumers to get the messages
+    connect(this, &VisClient::messageReceived, &consumer, &ConsumerDispatcher::onMessageReceived);
+    // hide the icons at the dispaly
+    consumer.off();
 
     qDebug() << "Create VIS client - send 0 to reset";
-
-    qDebug() << "!!!! RESET VALUES:" << mUrl;
-    for(int i = 5; i < aosVisParameters.size();++i)
-    {
-        ioctl(mFdept, i, 0);
-    }
-    ioctl(mFdept, CLUSTER_ACTIVE, 1);
 }
+
 VisClient::~VisClient()
 {
     qDebug() << "Delete VIS client";
-    close(mFdept);
     mWebSocket.close();
 }
 
@@ -157,9 +117,6 @@ void VisClient::onError(QAbstractSocket::SocketError error)
 void VisClient::onTextMessageReceived(const QString &message)
 {
     qDebug() << "Receive message:" << message;
-    // send dummy data 
-    struct taurus_cluster_data data = {0, CLUSTER_SPEED};
-    
     qDebug() << "mState: " << mState;
 
     switch(mState)
@@ -184,24 +141,7 @@ void VisClient::onTextMessageReceived(const QString &message)
             auto sId = getSubscriptionId(message);
             if(sId == mSubscriptionId)
             {
-                for (auto param : aosVisParameters)
-                {
-                    auto value = param.getValue(param.paramName, message);
-                    if(value != not_defined_value)
-                    {
-                        data.value = value;
-                        data.ioctl_cmd = param.ctlIOId;
-                        auto res = ioctl(mFdept, data.ioctl_cmd, data.value);
-                    }
-                    else 
-                    {
-                        qDebug() << "Not processed parameter:" << param.paramName;
-                    }
-                }
-            }
-            else
-            {
-                qDebug("Wrong subscription received.");
+                messageReceived(message);
             }
        }
        break;
@@ -211,95 +151,6 @@ void VisClient::onTextMessageReceived(const QString &message)
            return;
        }
     }
-}
-
-int VisClient::getTurnDirection(const QString & propId, const QString & message)
-{
-    auto value = getStringValue("Signal.Traffic.Turn.Direction", message);
-    if(value == "right")
-    {
-       return 1;
-    }
-    else if(value == "left")
-    {
-        return 2;
-    }
-    return not_defined_value;
-}
-
-QString VisClient::getStringValue(const QString & propId, const QString & message)
-{
-    /*QString res;
-    QByteArray br = message.toUtf8();
-    QJsonDocument doc = QJsonDocument::fromJson(br);
-    QJsonObject obj = doc.object();
-    QJsonArray arr = obj.value("value").toArray();
-    foreach(const QJsonValue &v, arr){
-        if(v.toObject().contains(propId)) {
-           res = v.toObject().value(propId).toString();
-       }
-    }
-    return res;*/
-    QByteArray br = message.toUtf8();
-    QJsonDocument doc = QJsonDocument::fromJson(br);
-    if(doc["value"].isObject() && doc["value"][propId].isBool())
-    {
-        return doc["value"][propId].toBool();
-    }
-    return "";
-}
-
-int VisClient::getBool(const QString & propId, const QString & message)
-{
-    int res = not_defined_value;
-    QByteArray br = message.toUtf8();
-
-    QJsonDocument doc = QJsonDocument::fromJson(br);
-    if(doc["value"].isObject() && doc["value"][propId].isBool())
-    {
-        res = (int)doc["value"][propId].toBool();
-    }
-    return res;
-}
-
-int VisClient::getBeltStatus(const QString & propId, const QString & message)
-{
-    auto res = getBool(propId, message);
-    if(res != not_defined_value)
-    {
-        res = !res;
-    }
-    return res;
-}
-
-int VisClient::getTireStatus(const QString & propId, const QString & message)
-{
-    int res = not_defined_value;
-    QByteArray br = message.toUtf8();
-
-    QJsonDocument doc = QJsonDocument::fromJson(br);
-    if(doc["value"].isObject() && !doc["value"][propId].isUndefined())
-    {
-        res = doc["value"][propId].toInt();
-        res = (int)(240 >= res);
-    }
-    return res;
-}
-
-int VisClient::getValue(const QString & propId, const QString & message)
-{
-    int res = not_defined_value;
-    QByteArray br = message.toUtf8();
-    QJsonDocument doc = QJsonDocument::fromJson(br);
-    QJsonObject obj = doc.object();
-    QJsonArray arr = obj.value("value").toArray();
-    foreach(const QJsonValue &v, arr){
-        if(v.toObject().contains(propId)) {
-            res = (int)(v.toObject().value(propId).toInt());
-	        qDebug()<< propId  << " = " << res;
-       }
-    }
-    return res;
 }
 
 QString VisClient::getSubscriptionId(const QString &message)
